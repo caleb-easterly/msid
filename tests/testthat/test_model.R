3d1e1ac52a2f28fa265732d8785530b2d5b1614c
Caleb Easterly <19418537+caleb-easterly@users.noreply.github.com>
2020-03-19 15:42:46 -0400

    add more model function tests

--- tests/testthat/test_model.R
+++ tests/testthat/test_model.R
@@ -1,8 +1,59 @@
 context("model")
 
-test_that("model runs, results > 0", {
-    data("contact_msid_base")
-    msid_parms <- define_parameters(contact_df = contact_msid_base, sexids = 'msid')
-    sted <- run_to_steady(msid_parms, rep(0, 12))
+model_test_fun1 <- function(rep, props, sexids) {
+    parms <- define_parameters(contact_df = rep,
+                               prop_df = props, sexids = sexids)
+    test_vacc <- define_vaccination(parms, 0, 0)
+    init_cond <- parms$structural$init_cond
+    output <- model_function(0, init_cond, parms, test_vacc)[[1]]
+
+    #### test 1: input and output vectors should be the same length
+    expect_equal(length(init_cond), length(output))
+
+    #### test 2: without vaccination, vacc compartments should be 0
+    v_ids <- parms$structural$model_indices %>% filter(epi == "V") %>%
+        select(model_uid) %>% unlist()
+
+    expect_equal(sum(output[v_ids]), 0)
+
+    #### test 3: when solved, no compartment should be negative
+    sted <- run_to_steady(parms, test_vacc)[2, -1]
     expect_true(all(sted > -.Machine$double.eps))
+
+    #### test 4: the sum should also be equal to 1, for all non-incidence compartments
+    non_inc <- parms$structural$model_indices %>% filter(epi != "I") %>%
+        select(model_uid) %>% unlist()
+    expect_equal(sum(sted[non_inc]), 1)
+}
+
+model_test_fun2 <- function(rep, props, sexids) {
+    parms <- define_parameters(contact_df = rep,
+                               prop_df = props, sexids = sexids)
+    test_vacc <- define_vaccination(parms, 0.5, 0.5)
+    sted <- run_to_steady(parms, test_vacc)[2, -1]
+
+    #### test 1: with vaccination, vacc compartments should be > 0
+    v_ids <- parms$structural$model_indices %>% dplyr::filter(epi == "V") %>%
+        select(model_uid) %>% unlist()
+    expect_true(all(sted[v_ids] > 0))
+}
+
+test_that("model runs, results appropriate", {
+    data("all_sexid_rep")
+    data("all_sexid_props")
+    data("het_rep")
+    data("het_props")
+    model_test_fun1(all_sexid_rep, all_sexid_props, 'msid')
+    model_test_fun1(het_rep, het_props, "het")
+    # model_test_fun1(sexid_tog_rep, sexid_tog_props, "msid_avg")
+})
+
+test_that("positive vaccination compartments w/ vacc", {
+    data("all_sexid_rep")
+    data("all_sexid_props")
+    model_test_fun2(all_sexid_rep, all_sexid_props, "msid")
+
+    data("het_rep")
+    data("het_props")
+    model_test_fun2(het_rep, het_props, "het")
 })
