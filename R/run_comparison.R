c2f0dbaf8f8a498a4cfc58bbcc4d0eb73695e782
Caleb Easterly <19418537+caleb-easterly@users.noreply.github.com>
2020-03-22 17:35:06 -0400

    add vacc level option and 0-vaccination data

--- R/run_comparison.R
+++ R/run_comparison.R
@@ -31,11 +31,12 @@ calibrate_model <- function(so, rep_df, prop_df, n_samp, n_resamp) {
 #' @param n_samp number of samples at each step in IMIS (\code{B} in \code{IMIS})
 #' @param n_resamp desired number of posterior samples (\code{B_k} in \code{IMIS()})
 #' @param n_run_samp samples from \code{<imis_results>$resample}
+#' @param vacc_level the desired coverage level. From 0 to 1.
 #'
 #' @importFrom IMIS IMIS
 #' @importFrom mvtnorm rmvnorm dmvnorm
 #' @export
-run_comparison <- function(so, rep_df, prop_df, calib_parms, n_run_samp){
+run_comparison <- function(so, rep_df, prop_df, calib_parms, n_run_samp, vacc_level){
     # todo: parallelize this part
     comps <- vector(mode = "list", length = n_run_samp)
     imis_n_samps <- nrow(calib_parms)
@@ -55,8 +56,11 @@ run_comparison <- function(so, rep_df, prop_df, calib_parms, n_run_samp){
         # use j to define the parms
         p <-  define_calib_parms(j, calib_parms, so, rep_df, prop_df)
 
-        vacc_level <- 0.5
         comp_endtime <- 20
+
+        # no vaccination
+        mf0 <- define_vaccination(p, m_vacc = 0, w_vacc = 0)
+        mf0_inf <- run_model_infections(p, mf0, endtime = comp_endtime)
 
         # 50% female vaccination
         f50 <- define_vaccination(p, m_vacc = 0, w_vacc = vacc_level)
@@ -69,6 +73,10 @@ run_comparison <- function(so, rep_df, prop_df, calib_parms, n_run_samp){
         # summarise cases by sex
         # multiply by 1000 so we get cases per 1000 people
         denom <- 1000
+        mf0_inf_sex <- mf0_inf %>%
+            group_by(sex) %>%
+            summarise(numc = sum(inf) * denom)
+
         f50_inf_sex <- f50_inf %>%
             group_by(sex) %>%
             summarise(numc = sum(inf) * denom)
@@ -78,10 +86,13 @@ run_comparison <- function(so, rep_df, prop_df, calib_parms, n_run_samp){
             summarise(numc = sum(inf) * denom)
 
         # calculate reduction
-        red <- left_join(f50_inf_sex, mf50_inf_sex, by = "sex",
-                         suffix = c(".f", ".mf")) %>%
-            mutate(abs_red = numc.f - numc.mf,
-                   rel_red = (numc.f - numc.mf) / numc.f * 100)
+        red <- left_join(mf0_inf_sex, f50_inf_sex, by = "sex",
+                         suffix = c(".0", ".f")) %>%
+            left_join(mf50_inf_sex, by = "sex") %>% rename(numc.mf = numc) %>%
+            mutate(abs_red_f_v_0 = numc.0 - numc.f,
+                   rel_red_f_v_0 = (numc.0 - numc.f) / numc.0 * 100,
+                   abs_red_mf_v_f = numc.f - numc.mf,
+                   rel_red_mf_v_f = (numc.f - numc.mf) / numc.f * 100)
         red$run <- i
         comps[[i]] <- list(prev, red)
     }
